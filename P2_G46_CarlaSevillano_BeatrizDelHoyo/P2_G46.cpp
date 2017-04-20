a6b77926cb0f5ed95da4450141ae76dffa435950
csevillano <csevillano@PB1-04>
2017-04-20 19:36:47 +0200

    Add project files.

---
+++ P2_G46_CarlaSevillano_BeatrizDelHoyo/P2_G46.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+
+
+
+
+int main()
+{
+	int counter = 0;
+	char aux[100];
+	std::string element {"hola"};
+	std::ifstream f_entrada_elements("elements.dat");
+
+	std::cout << element << std::endl;
+	char line[400];
+
+	while (f_entrada_elements.getline(line, 400))
+	{
+		do
+		{
+			aux[counter] = line[counter];
+			counter++;
+		} while (line[counter] != '=');
+		aux[counter] = '\0';
+		
+		element = "hola";
+		
+	};	f_entrada_elements.close();
+	return 0;
+}
