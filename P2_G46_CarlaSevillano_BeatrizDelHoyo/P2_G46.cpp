d041ddff1f9c5f48d07b08151d3b33578f8de273
Forzex96 <guillermoestebanplazas@enti.cat>
2017-05-01 20:36:46 +0200

    fgrtr

--- P2_G46_CarlaSevillano_BeatrizDelHoyo/P2_G46.cpp
+++ P2_G46_CarlaSevillano_BeatrizDelHoyo/P2_G46.cpp
@@ -20,4 +20,45 @@ bool IsNumber(std::string substring)
 int main()
 {
 
-	Game myGame;	myGame.Help();	std::string userInput;	do {		myGame.PrintElements();		getline(std::cin, userInput);		if (userInput == "help")		{			myGame.Help();		}		else if (userInput == "add basics")		{			myGame.AddBasics();		}		else if (userInput == "sort")		{			myGame.Sort();		}		else if (userInput == "clean")		{			myGame.Clean();		}		else if (userInput == "info")		{			//myGame.Info();		}		else if (userInput.substr(0, userInput.find(' ') - 1) == "add" && IsNumber(userInput.substr(userInput.find(' ') + 1)))		{			myGame.Add(stoi(userInput.substr(userInput.find(' ') + 1)));		}		}while (userInput!= "ESC");	return 0;}
+	Game myGame;
+
+	myGame.Help();
+	std::string userInput;
+	do {
+
+		myGame.PrintElements();
+		getline(std::cin, userInput);
+
+		if (userInput == "help")
+		{
+			myGame.Help();
+		}
+		else if (userInput == "add basics")
+		{
+			myGame.AddBasics();
+		}
+		else if (userInput == "sort")
+		{
+			myGame.Sort();
+		}
+		else if (userInput == "clean")
+		{
+			myGame.Clean();
+		}
+		else if (userInput == "info")
+		{
+			//myGame.Info();
+		}
+		else if (userInput.substr(0, userInput.find(' ') - 1) == "add" && IsNumber(userInput.substr(userInput.find(' ') + 1)))
+		{
+
+
+			myGame.Add(stoi(userInput.substr(userInput.find(' ') + 1)));
+		}
+
+	
+
+	}while (userInput!= "ESC");
+
+	return 0;
+}
